31dfc4c7d3c5e6f342e60b11600db6e4fb44b377
AdityaZxxx <adityaofficial714@gmail.com>
2025-01-01 18:47:38 +0700

    cange navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,9 +24,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`absolute z-10 top-4 md:top-6 left-1/2 transform -translate-x-1/2 w-full max-w-4xl shadow-lg bg-white dark:bg-gray-950 duration-500 dark:shadow-gray-900 flex justify-center items-center py-2 px-5 transition-all md:rounded-b-xl sm:rounded-none border border-gray-200 dark:border-gray-700 ${
-        isOpen ? '' : ''
-      }`}
+      className={`absolute z-10 top-4 md:top-6 left-1/2 transform -translate-x-1/2 w-full max-w-4xl  bg-transparent duration-500  flex justify-center items-center py-2 px-5  md:rounded-b-xl sm:rounded-none`}
     >
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
@@ -49,7 +47,7 @@ const Navbar = () => {
                   className={`text-gray-800 dark:text-gray-300 flex items-center duration-500 hover:scale-110 ${
                     pathname === item.href
                       ? 'underline text-gray-900 dark:text-white' // Highlight active link
-                      : 'relative text-black hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]'
+                      : 'relative text-black hover:text-gray-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]'
                   }`}
                 >
                   {item.label}
@@ -70,7 +68,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden h-screen absolute flex top-full font-semibold mt-0 left-0 w-full flex-col items-center backdrop-blur-lg shadow-b-lg dark:shadow-b-sm dark:shadow-gray-500 p-4 rounded-lg transition-transform duration-500 ease-in-out py-6 pt-20 ${
+        className={`md:hidden h-screen absolute flex top-full font-semibold mt-0 left-0 w-full flex-col items-center  bg-white dark:bg-gray-950 p-4 rounded-lg transition-transform duration-500 ease-in-out py-6 pt-20 ${
           isOpen
             ? 'scale-100 opacity-100 pointer-events-auto'
             : 'scale-75 opacity-0 pointer-events-none'
@@ -80,9 +78,9 @@ const Navbar = () => {
           <Link
             key={item.href}
             href={item.href}
-            className={`text-gray-600 dark:text-gray-300 text-xl duration-500 hover:scale-110 flex items-center py-2 ${
+            className={`text-gray-500 dark:text-gray-300 text-xl duration-500 hover:scale-110 flex items-center py-2 ${
               pathname === item.href
-                ? ' text-black dark:text-white' // Highlight active link
+                ? ' text-black underline underline-offset-8 dark:text-white scale-100' // Highlight active link
                 : 'relative text-black hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]'
             }`}
           >
