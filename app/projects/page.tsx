71b291065cc5619a51560623f42a6a5b31e8b549
AdityaZxxx <adityaofficial714@gmail.com>
2025-01-01 18:06:12 +0700

    add bg project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,9 @@ import ProjectCard from '@/components/ProjectCard'
 import { projects } from '../../data/projects'
 import Footer from '@/components/Footer'
 
+import AnimatedGridPattern from '@/components/ui/animated-grid-pattern'
+import { cn } from '@/lib/utils'
+
 export default function Projects() {
   return (
     <div className="min-h-screen mt-28  py-16 px-6 sm:px-8 md:px-16 lg:px-24">
@@ -16,11 +19,22 @@ export default function Projects() {
         </div>
 
         {/* Projects Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 md:mb-20">
+
+        <div className="grid grid-cols-1 z-10 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 md:mb-20">
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
+        <AnimatedGridPattern
+          numSquares={30}
+          maxOpacity={0.3}
+          duration={2}
+          repeatDelay={1}
+          className={cn(
+            '[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]',
+            'inset-x-0 overflow-hidden md:inset-y-[20%] inset-y-[70%] -z-10 skew-y-12 mt-5',
+          )}
+        />
       </div>
       <Footer seeMoreText="Let's Go To My Resume." seeMoreLink="/resume" />
     </div>
